0aa90b7baab03c4b66c875863340b5ff6ac2b8e3
Dmitrii Akimov <akimov.d.94@yandex.ru>
2022-05-10 05:24:19 +0300

    new class Cat, add @bean cat, add comparison of beans by link

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean1 == bean2);
+
+        System.out.println(cat1.getMessage());
+        System.out.println(cat2.getMessage());
+        System.out.println(cat1 == cat2);
     }
 }
